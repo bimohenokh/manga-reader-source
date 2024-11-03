ecbfdda054ba96a28981c9e67d93759e38abb3f9
bimohenokh <bimo.barata2@gmail.com>
2024-11-03 21:48:08 +0700

    init

---
+++ manga-reader-be/src/main/java/com/example/mangareaderbe/MangaReaderBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.mangareaderbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaReaderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangaReaderBeApplication.class, args);
+	}
+
+}
---
+++ manga-reader-be/src/test/java/com/example/mangareaderbe/MangaReaderBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mangareaderbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangaReaderBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
